21cdad048035034547cbbfaaa61368042437e280
Shaleen Jain <shaleen@jain.sh>
2019-08-03 16:24:32 +0530

    update truffle.js

--- truffle.js
+++ truffle.js
@@ -18,11 +18,11 @@
  *
  */
 
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "fj4jll3k.....";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
@@ -60,14 +60,32 @@ module.exports = {
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-      // network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/3bd97a52c6174632a2e5a9d9845ff114`),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+    kovan: {
+      provider: () => new HDWalletProvider(mnemonic, `https://kovan.infura.io/v3/3bd97a52c6174632a2e5a9d9845ff114`),
+      network_id: 42,
+      gas: 7000000,
+      gasPrice: 20000000000,
+      confirmations: 0,
+      timeoutBlocks: 500,
+      skipDryRun: true,
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/3bd97a52c6174632a2e5a9d9845ff114`),
+      network_id: 4,
+      gas: 6900000,
+      gasPrice: 25000000000,
+      confirmations: 1,
+      timeoutBlocks: 500,
+      skipDryRun: true,
+    },
 
     // Useful for private networks
     // private: {
@@ -79,21 +97,22 @@ module.exports = {
 
   // Set default mocha options here, use special reporters etc.
   mocha: {
-    // timeout: 100000
+    useColors: true,
+     timeout: 100000
   },
 
   // Configure your compilers
   compilers: {
     solc: {
       // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
+      // docker: true,
     }
-  }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+    evmVersion: 'byzantium',
+  },
 }
