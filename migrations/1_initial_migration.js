709febbc18c64fe4ddb70469c33b789d9c753441
Shaleen Jain <shaleen@jain.sh>
2019-08-03 11:55:40 +0530

    update migration script with dependency linking

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,23 @@
 const Migrations = artifacts.require("Migrations");
+const Publisher = artifacts.require("Publisher");
 const VoteCoin = artifacts.require("ERC721");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
-  deployer.deploy(VoteCoin);
+
+  let voteCoinContract, publisherContract;
+
+  // Deploy the VoteCoin ERC721 token
+  deployer.deploy(VoteCoin)
+    .then(function(instance) {
+      voteCoinContract = instance;
+      // Get the deployed instance of Publisher
+      return deployer.deploy(Publisher);
+    })
+    .then(function(instance) {
+      publisherContract = instance;
+        // Set the instance of VoteCoin's address on Publisher
+        // via Publisher's setVoteCoin() function.
+      return publisherContract.setVoteCoin(voteCoinContract.address);
+    });
 };
